c0ac0ee1187da6247b549a9fa04bcfe479078629
maksym-panov <work.maksym.panov@gmail.com>
2024-02-05 16:06:29 +0200

    Added JS file with duplications

---
+++ Duplications.js
@@ -0,0 +1,59 @@
+const callback1 = async () => {
+  const a = 1;
+  const b = 2;
+  const c = 3;
+  const d = 4;
+  
+  const e = 11;
+  const f = 12;
+  const g = 13;
+  const h = 14;
+
+  const result = (a + b + c + d) - (e + f + g + h);
+  console.log(result);
+};
+
+const callback2 = async () => {
+  const a = 1;
+  const b = 2;
+  const c = 3;
+  const d = 4;
+  
+  const e = 11;
+  const f = 12;
+  const g = 13;
+  const h = 14;
+
+  const result = (a + b + c + d) - (e + f + g + h);
+  console.log(result);
+};
+
+const callback3 = async () => {
+  const a = 1;
+  const b = 2;
+  const c = 3;
+  const d = 4;
+  
+  const e = 11;
+  const f = 12;
+  const g = 13;
+  const h = 14;
+
+  const result = (a + b + c + d) - (e + f + g + h);
+  console.log(result);
+};
+
+const callback4 = async () => {
+  const a = 1;
+  const b = 2;
+  const c = 3;
+  const d = 4;
+  
+  const e = 11;
+  const f = 12;
+  const g = 13;
+  const h = 14;
+
+  const result = (a + b + c + d) - (e + f + g + h);
+  console.log(result);
+};
