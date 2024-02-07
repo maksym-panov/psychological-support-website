2e573b9d73980cccd9eee70f84ecead82252bf05
maksym-panov <work.maksym.panov@gmail.com>
2024-02-07 15:51:03 +0200

    Fixed pseudorandom numbers generation security hotspot

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,14 @@ const MIN_STEP_TIMEOUT = 1000;
 const DEFAULT_START_VALUE = 500;
 const STATISTICS_CHANGE_RATE = 30;
 
+const CRYPTO = window.crypto || window.msCrypto;
+
 const scrollToElement = (targetId) => {
     const element = document.getElementById(targetId);
     element.scrollIntoView({
         behavior: "smooth", 
         block: "center"
     });
-};
-
-const doStatisticValueUpdate = (targetElement) => () => {
-    const newValue = Number(targetElement.innerHTML) + Math.ceil(Math.random() * 20);
-    targetElement.innerHTML = newValue;
 };
 
 const animateStatisticValue = (elementId, startVal, stepTimeoutMs) => { // анімація зміни статистики
@@ -28,10 +25,19 @@ const animateStatisticValue = (elementId, startVal, stepTimeoutMs) => { // ан�
     targetElement.innerHTML = startVal;
 
     setInterval(doStatisticValueUpdate(targetElement), stepTimeoutMs);
+}; 
+
+const doStatisticValueUpdate = (targetElement) => () => {
+    const array = new Uint32Array(1);
+    CRYPTO.getRandomValues(array);
+    const generatedIncrement = Math.ceil(Math.abs(array[0]) % STATISTICS_CHANGE_RATE);
+
+    const newValue = +targetElement.innerHTML + generatedIncrement;
+    targetElement.innerHTML = newValue;
 };
 
 window.onload = () => {
     setTimeout(() => animateStatisticValue("count1", 893, 3000), 800);
-    setTimeout(() => animateStatisticValue("count2", 674, 3000), 2700);
-    setTimeout(() => animateStatisticValue("count3", 2748, 3000), 4300);
+    setTimeout(() => animateStatisticValue("count2", 674, 3000), 2000);
+    setTimeout(() => animateStatisticValue("count3", 2748, 3000), 3300);
 };
